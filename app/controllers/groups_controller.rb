64448dc3126a28751e2f3818f38df9f4c4a04a0c
JasemDunan <jasemvalencia@gmail.com>
2020-12-09 17:29:09 -0500

    Create partial view, update and destroy methods for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,12 +16,36 @@ class GroupsController < ApplicationController
 
   # POST /groups/
   def create
-    @group = Group.new(name: params[:group][:name])
+    @group = Group.new(group_params)
 
     if @group.save
       redirect_to @group
     else
       render :new
     end
+  end
+
+  def edit
+    @group=Group.find(params[:id])
+  end
+
+  #DELETE /groups/:id
+  def destroy
+    @group=Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path
+  end
+
+  #PUT /groups/:id
+  def update
+    @group=Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to @group
+    else
+      render :edit
+    end
+  end
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
