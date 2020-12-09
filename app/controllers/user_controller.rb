00a8d78f1ed664699752df0f314f27f7712a0c04
JasemDunan <jasemvalencia@gmail.com>
2020-12-09 00:47:43 -0500

    Create models and controllers

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,8 @@
 class UserController < ApplicationController
+  #get /articles
   def index
+    
   end
+
+  #get /users/:id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get 'transaction/index'
   get 'group/index'
-  get 'user/index'
   
-  root 'user#login'
+  get 'user/index'
+  get 'user/login'
+  get 'user/splash'
+  post 'user/splash'
+  post 'user/login'
+  
+  root 'user#splash'
 
 end
