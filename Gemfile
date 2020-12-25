5bd62f110f6f2a848a21d2c99b8bbe7050be71c3
JasemDunan <jasemvalencia@gmail.com>
2020-12-24 23:59:56 -0500

    Add groups test

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
-  gem 'capybara', '>= 2.15'
+  # gem 'capybara', '>= 2.15'
   gem 'rspec-rails', '~> 4.0.1'
 end
 
@@ -51,6 +51,7 @@ end
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
+  gem 'database_cleaner'
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
--- spec/features/group_spec.rb
+++ spec/features/group_spec.rb
@@ -5,18 +5,18 @@ RSpec.feature 'Group', type: :feature do
     before do
       @yaser = User.create(email: 'yaser@gmail.com', name: 'Yaser', password: '123456')
       visit 'http://localhost:3000/users/sign_in'
-    #   fill_in 'Email', with: 'yaser@gmail.com'
-    #   fill_in 'Password', with: '123456'
-    #   click_on 'Log in'
+      fill_in 'Email', with: 'yaser@gmail.com'
+      fill_in 'Password', with: '123456'
+      click_on 'Log in'
     end
 
     scenario 'Create a group' do
-    #     visit 'http://localhost:3000/groups/new'
-    #   fill_in 'group_name', with: 'This is Hans post'
-    #   click_on 'Save Group'
+      visit 'http://localhost:3000/groups/new'
+      fill_in 'group_name', with: 'Group Test'
+      click_on 'SAVE GROUP'
     # #   find(:xpath, '//form[@id="new_comment"][@action="/posts/1/comments"]//input[@id="comment_content"]').set('This a comment from Yaser')
     # #   find(:xpath, '//form[@id="new_comment"][@action="/posts/1/comments"]').click_on('Comment')
-    #   expect(page).to have_content("Image can't be blank")
+       expect(page).to have_content("Image can't be blank")
     end
   end
 end
