3ce779b7abf8ce32f787dd5aa1ae8b20472feabf
JasemDunan <jasemvalencia@gmail.com>
2021-01-19 23:37:47 -0500

    move validateuser to application-controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    def validate_user
+        return if user_signed_in?
+    
+        redirect_to new_user_session_path, notice: 'You must log in first'
+      end
 end
